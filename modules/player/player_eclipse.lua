da95d10178142a1c61073e7924fea277e87ed116
Mario Mancino <mario@mancino-net.de>
2015-09-16 13:15:36 +0200

    Fixed Player-Eclipse Module

--- modules/player/player_eclipse.lua
+++ modules/player/player_eclipse.lua
@@ -33,29 +33,30 @@ end
 
 -- eclipsePower - Balance bar position, ranges from -100(solar) to 100(lunar)
 function Player.eclipsePower(self)
-    --TODO: Check if manual conversion is needed! if not - also fix all other calls to UnitPower in this module!
-    if(Player.eclipseDirLunar(self)) then
-        return UnitPower("player", 8)
-    else
-        return -1 * UnitPower("player", 8)
-    end
+    return -1 * UnitPower("player", 8)
 end
 
 -- eclipsePhase - Current Eclipse Phase (1-4).
 function Player.eclipsePhase(self)
-    if Player.eclipseDirSolar(self) then
-        if Player.eclipsePower(self) > 0 then
-            return 2
+    if Player.eclipsePower(self) > 0 then
+        if GetEclipseDirection() == "moon" then
+            return 1
         else
-            return 3
+            return 2 
         end
     else
-        if Player.eclipsePower(self) > 0 then
-            return 1
+        if GetEclipseDirection() == "sun" then
+            return 3
         else
             return 4
         end
     end
+end
+function Player.eclipseSolar(self)
+    return Player.eclipsePhase(self) >= 3
+end
+function Player.eclipseLunar(self)
+    return Player.eclipsePhase(self) <= 2
 end
 
 -- eclipsePhaseDuration - Duration of each eclipse duration
@@ -69,52 +70,56 @@ end
 
 -- eclipseMax - Time until the next solar or lunar max.
 local timeToMax = {}
-timeToMax[1] = {0.0,0.8}
-timeToMax[2] = {0.8,1.0}
-timeToMax[3] = {0.0,0.8}
-timeToMax[4] = {0.8,1.0}
+timeToMax[1] = {1.0,-1.0}
+timeToMax[2] = {1.0,1.0}
+timeToMax[3] = {1.0,-1.0}
+timeToMax[4] = {1.0,1.0}
 function Player.eclipseMax(self)
     local phase = Player.eclipsePhase(self)
     local duration = Player.eclipsePhaseDuration(self)
     local energyPct = UnitPower("player", 8) / 100.0
-    return (duration*timeToMax[phase][0])+(duration*timeToMax[phase][1]*energyPct)
+    if energyPct < 0 then energyPct = energyPct * -1 end
+    return (duration*timeToMax[phase][1])+(duration*timeToMax[phase][2]*energyPct)
 end
 
 -- eclipseLunarMax - Time until the next lunar max is reached
 local timeToLunarMax = {}
-timeToLunarMax[1] = {0.0,0.8}
-timeToLunarMax[2] = {2.8,1.0}
-timeToLunarMax[3] = {1.8,1.0}
-timeToLunarMax[4] = {0.8,1.0}
+timeToLunarMax[1] = {1.0,-1.0}
+timeToLunarMax[2] = {3.0,1.0}
+timeToLunarMax[3] = {2.0,-1.0}
+timeToLunarMax[4] = {1.0,1.0}
 function Player.eclipseLunarMax(self)
     local phase = Player.eclipsePhase(self)
     local duration = Player.eclipsePhaseDuration(self)
     local energyPct = UnitPower("player", 8) / 100.0
-    return (duration*timeToLunarMax[phase][0])+(duration*timeToLunarMax[phase][1]*energyPct)
+    if energyPct < 0 then energyPct = energyPct * -1 end
+    return (duration*timeToLunarMax[phase][1])+(duration*timeToLunarMax[phase][2]*energyPct)
 end
 
 --eclipseSolarMax - Time until the next solar max is reached
 local timeToSolarMax = {}
-timeToSolarMax[1] = {1.8,1.0}
-timeToSolarMax[2] = {0.8,1.0}
-timeToSolarMax[3] = {0.0,0.8}
-timeToSolarMax[4] = {2.8,1.0}
+timeToSolarMax[1] = {3.0,-1.0}
+timeToSolarMax[2] = {1.0,1.0}
+timeToSolarMax[3] = {1.0,-1.0}
+timeToSolarMax[4] = {3.0,1.0}
 function Player.eclipseSolarMax(self)
     local phase = Player.eclipsePhase(self)
     local duration = Player.eclipsePhaseDuration(self)
     local energyPct = UnitPower("player", 8) / 100.0
-    return (duration*timeToSolarMax[phase][0])+(duration*timeToSolarMax[phase][1]*energyPct)
+    if energyPct < 0 then energyPct = energyPct * -1 end
+    return (duration*timeToSolarMax[phase][1])+(duration*timeToSolarMax[phase][2]*energyPct)
 end
 
 -- eclipseChange - Time until eclipse energy hits 0
 local timeToChange = {}
-timeToChange[1] = {1.0,1.0}
+timeToChange[1] = {2.0,-1.0}
 timeToChange[2] = {0.0,1.0}
-timeToChange[3] = {1.0,1.0}
+timeToChange[3] = {2.0,-1.0}
 timeToChange[4] = {0.0,1.0}
 function Player.eclipseChange(self)
     local phase = Player.eclipsePhase(self)
     local duration = Player.eclipsePhaseDuration(self)
     local energyPct = UnitPower("player", 8) / 100.0
-    return (duration*timeToChange[phase][0])+(duration*timeToChange[phase][1]*energyPct)
+    if energyPct < 0 then energyPct = energyPct * -1 end
+    return (duration*timeToChange[phase][1])+(duration*timeToChange[phase][2]*energyPct)
 end
