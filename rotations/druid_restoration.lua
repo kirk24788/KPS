43463eaa9f45f0771d7952113c8c621ae3e8ea5c
SoulBandit <mohip1987@gmail.com>
2016-12-11 13:09:19 +0200

    Update druid_restoration.lua

--- rotations/druid_restoration.lua
+++ rotations/druid_restoration.lua
@@ -1,35 +1,51 @@
 --[[[
 @module Druid Restoration Rotation
-@author Kirk24788
+@author Kirk24788.xvir.subzrk
 @version 7.0.3
 ]]--
 local spells = kps.spells.druid
 local env = kps.env.druid
 
-kps.rotations.register("DRUID","RESTORATION",{
+kps.rotations.register("DRUID","RESTORATION",
+{
     -- Cooldowns
     {{"nested"}, 'kps.cooldowns and not player.isMoving', {
-        {spells.tranquility, 'player.mana < 0.9'},
-        {spells.innervate, 'not player.isMoving and heal.averageHpIncoming < 0.8'},
+        {spells.innervate, 'player.mana < 0.7'},
+        {spells.essenceOfGhanir, 'player.mana < 0.7'},
+        {spells.tranquility, 'not player.isMoving and heal.averageHpIncoming < 0.8'},
     }},
+	
+    -- Have Innervate Buff
     {{"nested"}, 'player.hasBuff(spells.innervate)', {
         {spells.regrowth, 'heal.defaultTank.hp < 0.6', kps.heal.defaultTank},
         {spells.regrowth, 'heal.lowestInRaid.hp < 0.8', kps.heal.lowestInRaid},
-        {spells.regrowth, 'heal.defaultTank.hp < 0.9', kps.heal.defaultTank},
+        {spells.regrowth, 'heal.defaultTank.hp < 0.8', kps.heal.defaultTank},
     }},
-
-    {spells.swiftmend, 'heal.defaultTarget.hp < 0.85 and (heal.defaultTarget.hasBuff(spells.rejuvination) or heal.defaultTarget.hasBuff(spells.regrowth))', kps.heal.defaultTarget },
-    {spells.lifebloom, 'heal.defaultTank.myBuffDuration(spells.lifebloom) < 3 and heal.defaultTank.hp < 1', kps.heal.defaultTank},
+	
+    -- Def CD's
+    {{"nested"}, 'kps.defensive', {
+		{spells.renewal, 'player.hp < 0.2'},
+		{spells.ironbark, 'player.hp < 0.4'},
+		{spells.barkskin, 'player.hp < 0.6'},
+        { {"macro"}, 'kps.useBagItem and player.hp < 0.8', "/use Healthstone" },		
+    }},
+	
+    {spells.regrowth, 'heal.defaultTank.hp < 0.5', kps.heal.defaultTank},
+    {spells.regrowth, 'heal.lowestInRaid.hp < 0.5', kps.heal.lowestInRaid},
+    {spells.regrowth, 'heal.defaultTarget.hp < 0.5', kps.heal.defaultTarget},
+	
+    {spells.swiftmend, 'heal.defaultTank.hp < 0.7', kps.heal.defaultTank},
+    {spells.swiftmend, 'heal.lowestInRaid.hp < 0.7', kps.heal.lowestInRaid},
+    {spells.swiftmend, 'heal.defaultTarget.hp < 0.7', kps.heal.defaultTarget},
+	
+    {spells.lifebloom, 'heal.defaultTank.myBuffDuration(spells.lifebloom) < 3', kps.heal.defaultTank},
+	
     {spells.wildGrowth, 'kps.multiTarget and heal.defaultTarget.hpIncoming < 0.9 and heal.defaultTarget.hp < 1', kps.heal.defaultTarget},
+    {spells.efflorescence, 'kps.multiTarget and heal.defaultTarget.hpIncoming < 0.9 and heal.defaultTarget.hp < 1', kps.heal.defaultTarget},
+	
     {spells.rejuvenation, 'heal.defaultTarget.buffDuration(spells.rejuvenation) < 3 and heal.defaultTarget.hp < 1', kps.heal.defaultTarget},
-    {spells.regrowth, 'heal.defaultTarget.hpIncoming < 0.5', kps.heal.defaultTarget},
-
-    {spells.naturesSwiftness, 'heal.defaultTarget.hpIncoming < 0.4', kps.heal.defaultTarget},
-
-    {{"nested"}, '(player.hasBuff(spells.naturesSwiftness) or heal.defaultTarget.hp < 0.6) and not player.isMoving', {
-        {spells.regrowth, 'player.hasBuff(spells.clearcasting)', kps.heal.defaultTarget},
-        {spells.healingTouch, 'heal.defaultTarget.hp < 1', kps.heal.defaultTarget},
-    }},
-
     {spells.rejuvenation, 'heal.lowestInRaid.myBuffDuration(spells.rejuvenation) < 3 and heal.lowestInRaid.hp < 1', kps.heal.lowestInRaid},
-}, "Legacy Rotation")
+	
+    {spells.healingTouch, 'heal.defaultTarget.hp < 0.9', kps.heal.defaultTarget},
+}
+,"Icy Veins")
