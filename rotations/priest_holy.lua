c5bb98723d46beeea2774eb35a2cbb87820a7f66
Subzrk <subzrk@gmail.com>
2016-12-17 16:46:25 +0200

    holy

--- rotations/priest_holy.lua
+++ rotations/priest_holy.lua
@@ -6,23 +6,6 @@
 local spells = kps.spells.priest
 local env = kps.env.priest
 
-
-kps.rotations.register("PRIEST","HOLY",
-{
-    {spells.powerInfusion, 'player.hasTalent(5, 2)'}, -- power_infusion,if=talent.power_infusion.enabled
-    {spells.shadowfiend, 'not player.hasTalent(3, 2)'}, -- shadowfiend,if=!talent.mindbender.enabled
-    {spells.mindbender, 'player.hasTalent(3, 2)'}, -- mindbender,if=talent.mindbender.enabled
-    {spells.shadowWordPain, 'not target.hasMyDebuff(spells.shadowWordPain)'}, -- shadow_word_pain,cycle_targets=1,max_cycle_targets=5,if=miss_react&!ticking
-    {spells.powerWordSolace}, -- power_word_solace
-    {spells.mindSear, 'activeEnemies.count >= 4'}, -- mind_sear,if=active_enemies>=4
-    {spells.holyFire}, -- holy_fire
-    {spells.smite}, -- smite
-    {spells.holyWordChastise}, -- holy_word,moving=1
-    {spells.shadowWordPain}, -- shadow_word_pain,moving=1
-}
-,"Holy damage")
-
-
 kps.rotations.register("PRIEST","HOLY",
 {
     
@@ -42,20 +25,47 @@ kps.rotations.register("PRIEST","HOLY",
 	-- surge Of Light Proc
     {{"nested"}, 'player.hasBuff(spells.surgeOfLight)', {
 	{spells.flashHeal, 'heal.defaultTank.hp < 0.4', kps.heal.defaultTank},
-	{spells.flashHeal, 'not heal.defaultTank.hp < 0.4', kps.heal.lowestInRaid},
+	{spells.flashHeal, 'heal.defaultTank.hp < 0.45', kps.heal.lowestInRaid},
     }},
+	-- Holy Word: Serenity
+	{spells.holyWordSerenity, 'heal.defaultTank.hp < 0.4', kps.heal.defaultTank},
+	{spells.holyWordSerenity, 'heal.lowestInRaid.hp < 0.3 and not heal.defaultTank.hp > 0.45', kps.heal.lowestInRaid},
+	{spells.holyWordSerenity, 'heal.defaultTarget.hp < 0.3 and heal.defaultTank.hp > 0.45', kps.heal.defaultTarget},
+	-- Prayer of Mending (Tank only)
+	{spells.prayerOfMending, kps.heal.defaultTank},
+	-- Flash Heal
+	{spells.flashHeal, 'heal.defaultTank.hp < 0.8', kps.heal.defaultTank},
+	{spells.flashHeal, 'heal.lowestInRaid.hp < 0.65', kps.heal.lowestInRaid},
+	{spells.flashHeal, 'heal.defaultTarget.hp < 0.65', kps.heal.defaultTarget},
+	-- Heal
+	{spells.heal, 'heal.defaultTank.hp < 0.95', kps.heal.defaultTank},
+	{spells.heal, 'heal.lowestInRaid.hp < 0.85', kps.heal.lowestInRaid},
+	{spells.heal, 'heal.defaultTarget.hp < 0.85', kps.heal.defaultTarget},
+	-- renew
+	{spells.renew, 'heal.defaultTank.hp < 1 and heal.defaultTank < 0.4 and heal.lowestInRaid.myBuffDuration(spells.renew) < 2', kps.heal.defaultTank},
+	{spells.renew, 'heal.lowestInRaid.hp < 0.9  and heal.defaultTank < 0.4 and not heal.lowestInRaid.hasBuff(spells.renew', kps.heal.lowestInRaid},
+	{spells.renew, 'heal.defaultTarget.hp < 0.9  and heal.defaultTank < 0.4 and heal.lowestInRaid.myBuffDuration(spells.renew)', kps.heal.defaultTarget},
+	-- smite when doing nothing
+	{spells.smite},
 	
-	{spells.heal, 'heal.defaultTank.hp < 0.9', kps.heal.defaultTank},
-	{spells.heal, 'heal.lowestInRaid.hp < 0.8', kps.heal.lowestInRaid},
-	{spells.heal, 'heal.defaultTarget.hp < 0.8', kps.heal.defaultTarget},
 	
-	{spells.renew, 'heal.defaultTank.hp < 1', kps.heal.defaultTank},
-	{spells.renew, 'heal.lowestInRaid.hp < 0.65', kps.heal.lowestInRaid},
-	{spells.renew, 'heal.defaultTarget.hp < 0.65', kps.heal.defaultTarget},
-	
-	{spells.flashHeal, 'heal.defaultTank.hp < 0.65', kps.heal.defaultTank},
-	{spells.flashHeal, 'heal.lowestInRaid.hp < 0.45', kps.heal.lowestInRaid},
-	{spells.flashHeal, 'heal.defaultTarget.hp < 0.45', kps.heal.defaultTarget},
 	
 }
 ,"Holy heal")
+
+kps.rotations.register("PRIEST","HOLY",
+{
+    {spells.powerInfusion, 'player.hasTalent(5, 2)'}, -- power_infusion,if=talent.power_infusion.enabled
+    {spells.shadowfiend, 'not player.hasTalent(3, 2)'}, -- shadowfiend,if=!talent.mindbender.enabled
+    {spells.mindbender, 'player.hasTalent(3, 2)'}, -- mindbender,if=talent.mindbender.enabled
+    {spells.shadowWordPain, 'not target.hasMyDebuff(spells.shadowWordPain)'}, -- shadow_word_pain,cycle_targets=1,max_cycle_targets=5,if=miss_react&!ticking
+    {spells.powerWordSolace}, -- power_word_solace
+    {spells.mindSear, 'activeEnemies.count >= 4'}, -- mind_sear,if=active_enemies>=4
+    {spells.holyFire}, -- holy_fire
+    {spells.smite}, -- smite
+    {spells.holyWordChastise}, -- holy_word,moving=1
+    {spells.shadowWordPain}, -- shadow_word_pain,moving=1
+}
+,"Holy damage")
+
+
