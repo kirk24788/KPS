71fd9a86d51576fc11f980e5f8c98e57dafd3c96
Mario Mancino <mario@mancino-net.de>
2016-12-28 08:31:51 +0100

    Fixed Global Spells

--- utils/printGlobalSpells.py
+++ utils/printGlobalSpells.py
@@ -18,11 +18,8 @@ spells.cc.add_spell(605) # Mind Control
 ## Druid
 spells.cc.add_spell(339) # Entangling Roots
 spells.cc.add_spell(33786) # Cyclone
-spells.cc.add_spell(2637) # Hibernate
 ## Hunter
-spells.cc.add_spell(1499) # Freezing Trap
 spells.cc.add_spell(13809) # FrosSnake Trapt Trap
-spells.cc.add_spell(34600) #
 ## Mage
 spells.cc.add_spell(118) # Polymorph
 spells.cc.add_spell(61305, "BlackCat") # Polymorph: Black Cat
@@ -56,13 +53,14 @@ spells.poly.add_spell(51514) # Hex
 
 # Spells which require a select (cast ond ground) - usually AE Spells
 spells.group("ae","Spells which require a select (cast ond ground) - usually AE Spells")
-spells.ae.add_all_spells([109248,30283,88685,724,32375,43265,62618,2120,115180,114158,73921,88747, 115313, 115460, 114192, 6544, 33395, 116011, 115315, 152108,61882])
+spells.ae.add_all_spells([109248,30283,32375,43265,62618,2120,114158,73921, 115313, 6544, 33395, 116011, 115315, 152108,61882])
 spells.ae.add_spell(5740,"Channeled") # Rain of Fire (Channeled)
-spells.ae.add_spell(104233,"Instant") # Rain of Fire (Instant)
 spells.ae.add_spell(13813) # Explosive Trap
 spells.ae.add_spell(13809) # Ice Trap
-spells.ae.add_spell(1499) # Freezing Trap
 spells.ae.add_spell(152280) # Defile
+spells.ae.add_spell(187827) # Metamorphosis
+spells.ae.add_spell(191034) # Starfall
+spells.ae.add_spell(145205) # Efflorescence
 
 # User-Priorized Spells to ignore
 spells.group("ignore","User-Priorized Spells to ignore")
@@ -73,13 +71,10 @@ spells.ignore.add_spell(115450, comment="Detox (when casting Detox without any d
 spells.ignore.add_spell(119582, comment="Purifying Brew (having more than 1 chi, this can prevent using it twice in a row)")
 spells.ignore.add_spell(115008, comment="Chi Torpedo (same as roll)")
 spells.ignore.add_spell(101545, comment="Flying Serpent Kick (prevents you from landing as soon as you start 'flying')")
-spells.ignore.add_spell(115921, comment="Legacy of The Emperor")
-spells.ignore.add_spell(116781, comment="Legacy of the White Tiger")
 spells.ignore.add_spell(115072, comment="Expel Harm (below 35%, brewmasters ignores cooldown on this spell)")
 spells.ignore.add_spell(115181, comment="Breath of Fire (if you are chi capped, this can make you burn all your chi)")
 spells.ignore.add_spell(115546, comment="Provoke (prevents you from wasting your taunt)")
 spells.ignore.add_spell(116740, comment="Tigereye Brew (prevents you from wasting your stacks and resetting your buff)")
-spells.ignore.add_spell(115294, comment="Mana Tea (This isn't an instant cast, but since it only has a 0.5 channeled time, it can triggers twice in the rotation)")
 spells.ignore.add_spell(111400, comment="warlock burning rush")
 spells.ignore.add_spell(108978, comment="Alter Time")
 spells.ignore.add_spell(12051, comment="Evocation")
