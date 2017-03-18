03f2edceb9a08378524568fc68a56e72e18b61eb
htordeux <glandbleu@imac-de-glandbleu.home>
2017-03-18 09:48:23 +0100

    Update
    functions usage in spelltable

--- rotations/priest.lua
+++ rotations/priest.lua
@@ -7,6 +7,46 @@ Priest Environment Functions and Variables.
 kps.env.priest = {}
 
 local MindFlay = tostring(kps.spells.priest.mindFlay)
+local VoidFormBuff = tostring(kps.spells.priest.voidform)
+
+local UnitDebuffDuration = function(spell,unit)
+	--if unit == nil then return "target" end
+	local spellname = tostring(spell)
+	local name,_,_,_,_,duration,endTime,caster,_,_ = UnitDebuff(unit,spellname)
+	if caster ~= "player" then return 0 end
+	if endTime == nil then return 0 end
+	local timeLeft = endTime - GetTime()
+    if timeLeft < 0 then return 0 end
+	return timeLeft
+end
+
+function UnitHasBuff(spell,unit)
+	--if unit == nil then unit = "player" end
+	local spellname = tostring(spell)
+	if spellname == nil then return false end
+	if select(1,UnitBuff(unit,spellname)) then return true end
+	return false
+end
+
+local function HealthPct(unit)
+	return UnitHealth(unit) / UnitHealthMax(unit)
+end
+local function PlayerHasTalent(row,talent)
+	local _, talentRowSelected =  GetTalentTierInfo(row,1)
+	if talent == talentRowSelected then return true end
+	return false
+end
+
+function kps.env.priest.canCastvoidBolt()
+	--if kps.multiTarget then return false end
+	if not UnitHasBuff(VoidFormBuff,"player") then return false end
+	if kps.spells.priest.voidEruption.cooldown > 0 then return false end
+	local Channeling = UnitChannelInfo("player") -- "Mind Flay" is a channeling spell
+	if Channeling ~= nil then
+	  if tostring(Channeling) == MindFlay then return true end
+	end
+	return false
+end
 
 function kps.env.priest.canCastMindBlast()
 	if kps.multiTarget then return false end
@@ -28,23 +68,14 @@ function kps.env.priest.testkps(arg)
 	return false
 end
 
-	--{spells.mindFlay, env.test }, -- true/false
-	--{spells.mindFlay, 'test()' }, -- lua error parser/lua:84 bad argument to unpack table expected got nil
-	--{spells.mindFlay, 'test(1)' }, -- true/false
-	
-	--{spells.mindFlay, 'testkps(kps.multiTarget)' }, -- true/false
-	--{spells.mindFlay, env.testkps(kps.multiTarget) }, -- always false
+--{spells.mindFlay, env.test }, -- true/false
+--{spells.mindFlay, 'test()' }, -- lua error parser/lua:84 bad argument to unpack table expected got nil
+--{spells.mindFlay, 'test(1)' }, -- true/false
 
-local PlayerDebuffDuration = function(spell,unit)
-	--if unit == nil then return "target" end
-	local spellname = tostring(spell)
-	local name,_,_,_,_,duration,endTime,caster,_,_ = UnitDebuff(unit,spellname)
-	if caster ~= "player" then return 0 end
-	if endTime == nil then return 0 end
-	local timeLeft = endTime - GetTime()
-    if timeLeft < 0 then return 0 end
-	return timeLeft
-end
+--{spells.mindFlay, 'testkps(kps.multiTarget)' }, -- true/false
+--{spells.mindFlay, env.testkps(kps.multiTarget) }, -- always false
+
+
 
 local Enemy = { "target", "focus" ,"mouseover" }
 function kps.env.priest.VoidBoltTarget()
@@ -52,8 +83,8 @@ function kps.env.priest.VoidBoltTarget()
 	local VoidBoltTargetDuration = 24
 	for i=1,#Enemy do -- for _,unit in ipairs(EnemyUnit) do
 		local unit = Enemy[i]
-		local shadowWordPainDuration = PlayerDebuffDuration(kps.spells.priest.shadowWordPain,unit)
-		local vampiricTouchDuration = PlayerDebuffDuration(kps.spells.priest.vampiricTouch,unit)
+		local shadowWordPainDuration = UnitDebuffDuration(kps.spells.priest.shadowWordPain,unit)
+		local vampiricTouchDuration = UnitDebuffDuration(kps.spells.priest.vampiricTouch,unit)
 		if shadowWordPainDuration > 0 and vampiricTouchDuration > 0 then
 			local duration = math.min(shadowWordPainDuration,vampiricTouchDuration)
 			if duration < VoidBoltTargetDuration then
@@ -63,4 +94,52 @@ function kps.env.priest.VoidBoltTarget()
 		end
 	end
 	return VoidBoltTarget
+end
+
+
+
+function kps.env.priest.DeathEnemyTarget()
+	local deathEnemyTarget = nil
+	for i=1,#Enemy do -- for _,unit in ipairs(EnemyUnit) do
+		local unit = Enemy[i]
+		if PlayerHasTalent(4,2) and HealthPct(unit) < 0.35 then
+			deathEnemyTarget = unit
+		elseif HealthPct(unit) < 0.20 then
+			deathEnemyTarget = unit
+		break end
+	end
+	return deathEnemyTarget
+end
+
+local UnitIsUnit = UnitIsUnit
+local UnitExists = UnitExists
+
+local function UnitIsAttackable(unit)
+    if not UnitExists(unit) then return false end
+    if (string.match(GetUnitName(unit), kps.locale["Dummy"])) then return true end
+    if UnitCanAttack("player",unit) == false then return false end-- UnitCanAttack(attacker, attacked) return 1 if the attacker can attack the attacked, nil otherwise.
+    if UnitIsEnemy("player",unit) == false then return false end -- WARNING a unit is hostile to you or not Returns either 1 ot nil -- Raider's Training returns nil with UnitIsEnemy
+    --TODO: if jps.PlayerIsBlacklisted(self.unit) then return false end -- WARNING Blacklist is updated only when UNITH HEALTH occurs
+    if not kps.env.harmSpell.inRange(unit) then return false end
+    return true
+end
+
+function kps.env.priest.TargetMouseover()
+	-- Config FOCUS with MOUSEOVER
+	if not UnitExists("focus") and UnitIsAttackable("mouseover") then
+		if UnitIsUnit("mouseovertarget","player") and not UnitIsUnit("target","mouseover") then
+			kps.runMacro("/focus mouseover") --print("Enemy DAMAGER|cff1eff00 "..name.." |cffffffffset as FOCUS")
+		elseif not UnitIsUnit("target","mouseover") and UnitDebuffDuration(kps.spells.priest.shadowWordPain,"mouseover") < 2 then 
+			kps.runMacro("/focus mouseover") --print("Enemy COMBAT|cff1eff00 "..name.." |cffffffffset as FOCUS not DEBUFF")
+		elseif not UnitIsUnit("target","mouseover") and UnitDebuffDuration(kps.spells.priest.vampiricTouch,"mouseover") < 2 then
+			kps.runMacro("/focus mouseover") --print("Enemy COMBAT|cff1eff00 "..name.." |cffffffffset as FOCUS not DEBUFF")
+		elseif not UnitIsUnit("target","mouseover") then
+			kps.runMacro("/focus mouseover")
+		end
+	end
+	if UnitExists("focus") and UnitIsUnit("target","focus") then
+		kps.runMacro("/clearfocus")
+	elseif UnitExists("focus") and not UnitIsAttackable("focus") then
+		kps.runMacro("/clearfocus")
+	end
 end
--- rotations/priest_shadow.lua
+++ rotations/priest_shadow.lua
@@ -10,7 +10,9 @@ local env = kps.env.priest
 ----------------------------------------
 
 kps.rotations.register("PRIEST","SHADOW",{
-	
+
+	env.TargetMouseover,
+	-- "Dispersion" 47585
 	{spells.dispersion, 'player.hp < 0.40' },
 	{spells.fade, 'not player.isPVP and player.isTarget' },
 	-- "Power Word: Shield" 17
@@ -22,8 +24,10 @@ kps.rotations.register("PRIEST","SHADOW",{
 	-- "Don des naaru" 59544
 	{spells.giftNaaru, 'player.hp < 0.70', "player" },
 	-- "Etreinte vampirique" buff 15286 -- pendant 15 sec, vous permet de rendre à un allié proche, un montant de points de vie égal à 40% des dégâts d’Ombre que vous infligez avec des sorts à cible unique
-	{spells.vampiricEmbrace, 'player.hasBuff(spells.voidform) and player.hp < 0.60' },
+	{spells.vampiricEmbrace, 'player.hasBuff(spells.voidform) and player.hp < 0.50' },
 	{spells.vampiricEmbrace, 'player.hasBuff(spells.voidform) and heal.averageHpIncoming < 0.80' },
+	-- "Guérison de l’ombre" 186263 -- debuff "Shadow Mend" 187464 10 sec
+	{spells.shadowMend, 'not spells.shadowMend.lastCasted(4) and not player.isMoving and not player.hasBuff(spells.voidform) and player.hp < 0.60 and not playerHasBuff(spells.vampiricEmbrace)' , "player" },
 	
 	-- "Purify Disease" 213634
 	{{"nested"}, 'kps.cooldowns',{
@@ -40,56 +44,57 @@ kps.rotations.register("PRIEST","SHADOW",{
 		{spells.mindBomb, 'target.IsCasting and target.distance < 30' , "target" },
 		{spells.mindBomb, 'focus.IsCasting and focus.distance < 30' , "focus" },
 		{spells.mindBomb, 'kps.multiTarget' , "target" },
-	    -- "Levitate" 1706
-		{ spells.levitate, 'kps.defensive and player.isFallingFor(2) and not player.hasBuff(spells.levitate)' , "player" },
-		{ spells.levitate, 'kps.defensive and player.isSwimming and not player.hasBuff(spells.levitate)' , "player" },
     }},
-
-	-- "Guérison de l’ombre" 186263 -- debuff "Shadow Mend" 187464 10 sec
-	{spells.shadowMend, 'not spells.shadowMend.lastCasted(4) and not player.isMoving and not player.hasBuff(spells.voidform) and player.hp < 0.60 and not playerHasBuff(spells.vampiricEmbrace)' , "player" },
-
-    -- "Déferlante d’ombre" 205385
-    {spells.shadowCrash, 'player.hasTalent(7,2)' },
+    
+   	 -- "Levitate" 1706
+	{ spells.levitate, 'kps.defensive and player.isFallingFor(2) and not player.hasBuff(spells.levitate)' , "player" },
+	--{ spells.levitate, 'kps.defensive and player.isSwimming and not player.hasBuff(spells.levitate)' , "player" },
+	-- mindblast is highest priority spell out of voidform
+	{spells.mindBlast, 'not player.isMoving and not player.hasBuff(spells.voidform)' , "target"  },
 
 	{spells.voidEruption , 'target.isAttackable and not player.hasBuff(spells.voidform) and player.insanity == 100' },
-	{spells.voidEruption , 'target.isAttackable and not player.hasBuff(spells.voidform) and player.hasTalent(7,1) and player.insanity > 65' },
-	{spells.powerInfusion, 'player.buffStacks(spells.voidform) > 9 and player.insanity > 65' },
+	{spells.voidEruption , 'target.isAttackable and not player.hasBuff(spells.voidform) and player.hasTalent(7,1) and player.insanity > 64' },
+
+    -- "Shadow Word: Death" 32379
+    {spells.shadowWordDeath, 'spells.shadowWordDeath.charges == 2' , "target" },
+    {spells.shadowWordDeath, 'DeathEnemyTarget() ~= nil and player.insanity < 85' , env.DeathEnemyTarget },
+ 
+ 	-- "Infusion de puissance"  -- Confère un regain de puissance pendant 20 sec, ce qui augmente la hâte de 25%
+	-- "Mindbender" cd 1 min duration 12 sec -- "Ombrefiel" cd 3 min duration 12sec
+	{spells.powerInfusion, 'player.buffStacks(spells.voidform) > 14 and player.insanity > 64' },
 	{spells.shadowfiend, 'player.buffStacks(spells.voidform) > 9' },
 	{spells.mindbender, 'player.buffStacks(spells.voidform) > 9' },
-
-	{{"nested"}, 'player.hasBuff(spells.voidform)', {
-		{spells.voidEruption, 'target.myDebuffDuration(spells.shadowWordPain) > 0 and target.myDebuffDuration(spells.shadowWordPain) < focus.myDebuffDuration(spells.shadowWordPain)' , "target" },
---		{spells.voidEruption, 'target.myDebuffDuration(spells.vampiricTouch) > 0 and target.myDebuffDuration(spells.vampiricTouch) < focus.myDebuffDuration(spells.vampiricTouch)' , "target" },
---		{spells.voidEruption, 'mouseover.myDebuffDuration(spells.shadowWordPain) > 0 and mouseover.myDebuffDuration(spells.shadowWordPain) < target.myDebuffDuration(spells.shadowWordPain) and mouseover.myDebuffDuration(spells.shadowWordPain) < focus.myDebuffDuration(spells.shadowWordPain)' , "mouseover" },
---		{spells.voidEruption, 'mouseover.myDebuffDuration(spells.vampiricTouch) > 0 and mouseover.myDebuffDuration(spells.vampiricTouch) < target.myDebuffDuration(spells.vampiricTouch) and mouseover.myDebuffDuration(spells.vampiricTouch) < focus.myDebuffDuration(spells.vampiricTouch)' , "mouseover" },
---		{spells.voidEruption, 'focus.myDebuffDuration(spells.shadowWordPain) > 0 and focus.myDebuffDuration(spells.shadowWordPain) < target.myDebuffDuration(spells.shadowWordPain)' , "focus" },
---		{spells.voidEruption, 'focus.myDebuffDuration(spells.vampiricTouch) > 0 and focus.myDebuffDuration(spells.vampiricTouch) < target.myDebuffDuration(spells.vampiricTouch)' , "focus" },
-		{spells.voidEruption, 'true' , VoidBoltTarget },
-		{spells.voidTorrent, 'not player.isMoving' },
-	}},
-
-	{spells.shadowWordDeath, 'target.hp < 0.20' , "target" },
-	{spells.shadowWordDeath, 'player.hasTalent(4,2) and target.hp < 0.35' , "target" },
-	{spells.shadowWordDeath, 'focus.hp < 0.20' , "focus" },
-	{spells.shadowWordDeath, 'player.hasTalent(4,2) and focus.hp < 0.35' , "focus" },
-
-	--{{"macro"}, env.canCastMindBlast , "/stopcasting" },
-	{{"macro"}, 'canCastMindBlast()' , "/stopcasting" },
-	{spells.mindBlast, 'not player.isMoving' },
 	
 	{spells.vampiricTouch, 'not player.isMoving and target.myDebuffDuration(spells.vampiricTouch) < 4 and not spells.vampiricTouch.isRecastAt("target")' , 'target' },
 	{spells.shadowWordPain, 'target.myDebuffDuration(spells.shadowWordPain) < 4 and not spells.shadowWordPain.isRecastAt("target")' , 'target' },
 	{spells.vampiricTouch, 'not player.isMoving and focus.myDebuffDuration(spells.vampiricTouch) < 4 and not spells.vampiricTouch.isRecastAt("focus") ' , 'focus' },
 	{spells.shadowWordPain, 'focus.myDebuffDuration(spells.shadowWordPain) < 4 and not spells.shadowWordPain.isRecastAt("focus") ' , 'focus' },
 
+	{{"macro"}, 'canCastvoidBolt()' , "/stopcasting" },
+	{spells.voidEruption, 'player.hasBuff(spells.voidform) and VoidBoltTarget() ~= nil' , env.VoidBoltTarget },
+	{spells.voidTorrent, 'player.hasBuff(spells.voidform) and not player.isMoving' },
+	
+	{spells.vampiricTouch, 'kps.multiTarget and mouseover.isAttackable and not player.isMoving and mouseover.myDebuffDuration(spells.vampiricTouch) < 4 and not spells.vampiricTouch.isRecastAt("mouseover") ' , 'mouseover' },
+	{spells.shadowWordPain, 'kps.multiTarget and mouseover.isAttackable and mouseover.myDebuffDuration(spells.shadowWordPain) < 4 and not spells.shadowWordPain.isRecastAt("mouseover") ' , 'mouseover' },
+
+	--{{"macro"}, env.canCastMindBlast , "/stopcasting" },
+	{{"macro"}, 'canCastMindBlast()' , "/stopcasting" },
+	{spells.mindBlast, 'not player.isMoving' },
+
 	{spells.vampiricTouch, 'mouseover.inCombat and not player.isMoving and mouseover.myDebuffDuration(spells.vampiricTouch) < 4 and not spells.vampiricTouch.isRecastAt("mouseover") ' , 'mouseover' },
 	{spells.shadowWordPain, 'mouseover.inCombat and mouseover.myDebuffDuration(spells.shadowWordPain) < 4 and not spells.shadowWordPain.isRecastAt("mouseover") ' , 'mouseover' },
-	{spells.vampiricTouch, 'kps.defensive and mouseover.isAttackable and not player.isMoving and mouseover.myDebuffDuration(spells.vampiricTouch) < 4 and not spells.vampiricTouch.isRecastAt("mouseover") ' , 'mouseover' },
-	{spells.shadowWordPain, 'kps.defensive and mouseover.isAttackable and mouseover.myDebuffDuration(spells.shadowWordPain) < 4 and not spells.shadowWordPain.isRecastAt("mouseover") ' , 'mouseover' },
 
 	{spells.mindFlay, 'not player.isMoving' },
 }
 ,"Shadow Priest")
+
+--		{spells.voidEruption, 'target.myDebuffDuration(spells.shadowWordPain) > 0 and target.myDebuffDuration(spells.shadowWordPain) < focus.myDebuffDuration(spells.shadowWordPain)' , "target" },
+--		{spells.voidEruption, 'target.myDebuffDuration(spells.vampiricTouch) > 0 and target.myDebuffDuration(spells.vampiricTouch) < focus.myDebuffDuration(spells.vampiricTouch)' , "target" },
+--		{spells.voidEruption, 'mouseover.myDebuffDuration(spells.shadowWordPain) > 0 and mouseover.myDebuffDuration(spells.shadowWordPain) < target.myDebuffDuration(spells.shadowWordPain) and mouseover.myDebuffDuration(spells.shadowWordPain) < focus.myDebuffDuration(spells.shadowWordPain)' , "mouseover" },
+--		{spells.voidEruption, 'mouseover.myDebuffDuration(spells.vampiricTouch) > 0 and mouseover.myDebuffDuration(spells.vampiricTouch) < target.myDebuffDuration(spells.vampiricTouch) and mouseover.myDebuffDuration(spells.vampiricTouch) < focus.myDebuffDuration(spells.vampiricTouch)' , "mouseover" },
+--		{spells.voidEruption, 'focus.myDebuffDuration(spells.shadowWordPain) > 0 and focus.myDebuffDuration(spells.shadowWordPain) < target.myDebuffDuration(spells.shadowWordPain)' , "focus" },
+--		{spells.voidEruption, 'focus.myDebuffDuration(spells.vampiricTouch) > 0 and focus.myDebuffDuration(spells.vampiricTouch) < target.myDebuffDuration(spells.vampiricTouch)' , "focus" },
+		
 
 ----------------------------------------
 ----------------------------------------
